c212f67fd0fc2ffd789d130e58f60b81dd18abe4
Sawako Ishida <sawako.yoriko@gmail.com>
2016-04-24 11:55:22 -0700

    added bootstrap buttons

--- front-end/app/js/app.js
+++ front-end/app/js/app.js
@@ -23,6 +23,25 @@ var ContinentsData = React.createClass({
 		this.serverRequest.abort();
 	},
 
+	post: function(){
+		// this.serverRequest = $ajax({
+		// 	method: 'POST',
+		// 	url: 'http://localhost:3000/continents'
+		// 	data: {
+		// 	}
+		// }).done(function(data){
+		// 	console.log('Here is data : ' + data);
+		// });
+		alert('post triggered!');
+	},
+
+	edit: function() {
+		// this.serverRequest = $ajax({
+		// 	method: 'PUT',
+		// 	url: 'http://localhost:3000/continents/' + id
+		// });
+		alert('edit triggered!')
+	},
 
 	render: function(){
 		return (
@@ -31,9 +50,15 @@ var ContinentsData = React.createClass({
 					<li>{this.state.country}</li>
 					<li>{this.state.region}</li>
 					<li>{this.state.mineral}</li>
+					<button type="button" class="btn btn-success">GET</button>
+					<button onClick={this.post} type="button" class="btn btn-primary">POST</button>
+					<button onClick={this.edit} type="button" class="btn btn-warning">EDIT</button>
+					<button type="button" class="btn btn-danger">DELETE</button>
 				</ul>
 		)
 	}
 });
 
-ReactDOM.render(<ContinentsData />, document.getElementById('example'));
+ReactDOM.render(<ContinentsData />
+
+	, document.getElementById('example'));
