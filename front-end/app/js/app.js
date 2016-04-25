b71694b857a0913bb102bd5235cb224a19d9904f
Sawako Ishida <sawako.yoriko@gmail.com>
2016-04-24 23:25:14 -0700

    separated getall and put

--- front-end/app/js/app.js
+++ front-end/app/js/app.js
@@ -1,24 +1,31 @@
 var ContinentsData = React.createClass({
 	getInitialState: function(){
 		return {
-			country: '',
-			region: '',
-			mineral: ''
-		};
+			data: [{
+				id: '',
+				country: '',
+				region: '',
+				mineral: ''
+			}]
+			}
 	},
-
 	componentDidMount: function(){
 		this.serverRequest = $.ajax({
 			method: 'GET',
 			url: 'http://localhost:3000/continents',
 			cache: false
 		}).then(function(result){
-			console.dir('here is result ' + result);
-			this.setState({
-				country: result.country,
-				region: result.region,
-				mineral: result.mineral
-			});
+			console.dir('here is result ' + JSON.stringify(result));
+				var newState = this.setState(
+					{data: [{
+					id: result.data._id,
+					country: result.data.country,
+					region: result.data.region,
+					mineral: result.data.mineral
+			}]})
+
+			var splitData = newState.data.slice(',');
+			this.state.push(splitData)
 		}.bind(this));
 	},
 
@@ -26,26 +33,67 @@ var ContinentsData = React.createClass({
 		this.serverRequest.abort();
 	},
 
-	post: function(e){
+	render: function(){
+		console.log('State Array ' + JSON.stringify(this.state.data));
+
+			return (
+				<ul>
+					<h3>Continents:</h3>
+					<li >ID: {this.state.data._id}</li>
+					<li>Country: {this.state.data.country}</li>
+					<li>Region: {this.state.data.region}</li>
+					<li>Mineral: {this.state.data.mineral}</li>
+				</ul>
+			)
+
+		// return retunThisComp;
+	}
+});//end of continents data get all class
+
+var ContinentsDataSecond = React.createClass({
+	getFormState: function(e){
+		var self = this;
 		e.preventDefault();
 		debugger;
-		var valId = this.refs.newId.value;
-		var valCountry = this.refs.newCountry.value;
-		var valRegion = this.refs.newRegion.value;
-		var valMineral = this.refs.newMineral.value;
-		// this.serverRequest = $ajax({
-		// 	method: 'POST',
-		// 	url: 'http://localhost:3000/continents'
-		// 	data: {
-		// 	}
-		// }).done(function(data){
-		// 	console.log('Here is data : ' + data);
-		// });
+		var valId = e.target.value;
+		var valCountry = e.target.value;
+		var valRegion = e.target.value;
+		var valMineral = e.target.value;
 		console.log('ID : ' + valId);
 		console.log('COUNTRY : ' + valCountry);
 		console.log('REGION : ' + valRegion);
 		console.log('MINERAL : ' + valMineral);
-		return;
+		this.setState({
+				country: e.target.country.value,
+				region: e.target.region.value,
+				mineral: e.target.mineral.value
+		});
+		console.dir('State : ' + this.state.country);
+	},
+
+	post: function(e){
+		var self = this;
+		e.preventDefault();
+
+
+
+			dispatch
+			this.serverRequest = $.ajax({
+				method: 'POST',
+				url: 'http://localhost:3000/continents',
+				data: {
+					'country': JSON.stringify(this.refs.newCountry.value),
+					'region': JSON.stringify(this.refs.newRegion.value),
+					'mineral': JSON.stringify(this.refs.newMineral.value)
+				},
+				cache: false,
+				dataType: 'json',
+				success: function(xmlRequestObj, successString){
+					console.dir('Here is successString : ' + JSON.stringify(successString));
+					console.dir('Here is xmlRequestObj : ' + JSON.stringify(xmlRequestObj));
+				}.bind(this)
+			});
+
 	},
 
 	edit: function() {
@@ -58,25 +106,33 @@ var ContinentsData = React.createClass({
 
 	render: function(){
 		return (
+			<form className="ContinentsData" onSubmit={this.getFormState}>
 				<ul>
 					<h3>Continents:</h3>
 					<li>ID: {this.state.id}</li>
-					<textarea type="text" ref="newId" class="form-control"></textarea>
+					<textarea  value={this.state.id} ref="newId"></textarea>
 					<li>Country: {this.state.country}</li>
-					<textarea type="text" ref="newCountry" class="form-control"></textarea>
+					<textarea value={this.state.country} ref="newCountry"></textarea>
 					<li>Region: {this.state.region}</li>
-					<textarea type="text" ref="newRegion" class="form-control"></textarea>
+					<textarea value={this.state.region} ref="newRegion"></textarea>
 					<li>Mineral: {this.state.mineral}</li>
-					<textarea type="text" ref="newMineral" class="form-control"></textarea>
+					<textarea value={this.state.mineral} ref="newMineral"></textarea>
 					<button type="button" class="btn btn-success">GET</button>
-					<button onClick={this.post} type="button" class="btn btn-primary">POST</button>
+					<button type="submit" class="btn btn-primary">POST</button>
 					<button onClick={this.edit} type="button" class="btn btn-warning">EDIT</button>
 					<button type="button" class="btn btn-danger">DELETE</button>
 				</ul>
+			</form>
 		)
 	}
 });
-
-ReactDOM.render(<ContinentsData />
+ReactDOM.render(
+							<section>
+								<ContinentsData />
+								<ContinentsData />
+								<ContinentsData />
+								<ContinentsData />
+								<ContinentsData />
+							</section>
 
 	, document.getElementById('example'));
